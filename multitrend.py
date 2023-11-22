6b8d7ef009cc8bf42f36fab696faabbb4705952f
Cole Rottenberg <cole.rottenberg@gmail.com>
2023-11-22 12:38:02 -0500

    working on trend

--- multitrend.py
+++ multitrend.py
@@ -3,3 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from enum import Enum
 from scipy.stats import norm
+import sys
+
+from chapter1 import pd_readcsv
+from chapter3 import standardDeviation
+
+# NEED TO READ DATA FIRST
+
+# grab fisrt string from arg
+filename = sys.argv[1]
+data = pd_readcsv(filename)
+data = data.dropna()
