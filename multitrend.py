74bee809b545cb297aadd0e6f2b4cbd1e55b88af
Cole Rottenberg <cole.rottenberg@gmail.com>
2023-11-30 15:25:28 -0500

    isolating multitrend to single function

--- multitrend.py
+++ multitrend.py
@@ -11,7 +11,6 @@ from chapter1 import (
 )
 from chapter3 import standardDeviation
 from chapter4 import (
-    get_data_dict,
     create_fx_series_given_adjusted_prices_dict,
     calculate_variable_standard_deviation_for_risk_targeting_from_dict,
     calculate_position_series_given_variable_risk_for_dict,
@@ -20,82 +19,104 @@ from chapter4 import (
 from chapter5 import calculate_perc_returns_for_dict_with_costs
 from chapter7 import calculate_forecast_for_ewmac
 from chapter8 import apply_buffering_to_position_dict
+from chapter9 import (
+    calculate_position_dict_with_multiple_trend_forecast_applied,
+    calculate_position_with_multiple_trend_forecast_applied,
 
+)
 # NEED TO READ DATA FIRST
 
 # grab fisrt string from arg
+if len(sys.argv) < 2:
+    print("Usage: python3 multitrend.py <filename>")
+    sys.exit(1)
+
 filename = sys.argv[1]
 data = pd_readcsv(filename)
 data = data.dropna()
 
-def calculate_position_dict_with_multiple_trend_forecast_applied(
-    adjusted_prices_dict: dict,
-    average_position_contracts_dict: dict,
-    std_dev_dict: dict,
-    fast_spans: list,
-) -> dict:
+def get_data_dict(instr_list: list):
 
-    list_of_instruments = list(adjusted_prices_dict.keys())
-    position_dict_with_trend_filter = dict(
+    all_data = dict(
         [
-            (
-                instrument_code,
-                calculate_position_with_multiple_trend_forecast_applied(
-                    adjusted_prices_dict[instrument_code],
-                    average_position_contracts_dict[instrument_code],
-                    stdev_ann_perc=std_dev_dict[instrument_code],
-                    fast_spans=fast_spans,
-                ),
-            )
-            for instrument_code in list_of_instruments
+            (instrument_code, pd_readcsv("%s.csv" % instrument_code))
+            for instrument_code in instr_list
         ]
     )
 
-    return position_dict_with_trend_filter
-
-
-def calculate_position_with_multiple_trend_forecast_applied(
-    adjusted_price: pd.Series,
-    average_position: pd.Series,
-    stdev_ann_perc: standardDeviation,
-    fast_spans: list,
-) -> pd.Series:
-
-    forecast = calculate_combined_ewmac_forecast(
-        adjusted_price=adjusted_price,
-        stdev_ann_perc=stdev_ann_perc,
-        fast_spans=fast_spans,
+    adjusted_prices = dict(
+        [
+            (instrument_code, data_for_instrument.adjusted)
+            for instrument_code, data_for_instrument in all_data.items()
+        ]
     )
 
-    return forecast * average_position / 10
+    current_prices = dict(
+        [
+            (instrument_code, data_for_instrument.underlying)
+            for instrument_code, data_for_instrument in all_data.items()
+        ]
+    )
 
+    return adjusted_prices, current_prices
 
-def calculate_combined_ewmac_forecast(
-    adjusted_price: pd.Series,
-    stdev_ann_perc: standardDeviation,
-    fast_spans: list,
-) -> pd.Series:
+def trend_forecast(risk_target_tau: float, multipliers: dict, instr_list: list, fast_span: list) -> dict:
 
-    all_forecasts_as_list = [
-        calculate_forecast_for_ewmac(
-            adjusted_price=adjusted_price,
-            stdev_ann_perc=stdev_ann_perc,
-            fast_span=fast_span,
+    adjusted_prices_dict, current_prices_dict = get_data_dict(instr_list)
+
+    fx_series_dict = create_fx_series_given_adjusted_prices_dict(adjusted_prices_dict)
+
+    capital = 1000000
+
+    idm = 1.5
+    instrument_weights = dict(sp500=0.5, us10=0.5)
+    cost_per_contract_dict = dict(sp500=0.875, us10=5)
+
+    std_dev_dict = calculate_variable_standard_deviation_for_risk_targeting_from_dict(
+        adjusted_prices=adjusted_prices_dict, current_prices=current_prices_dict
+    )
+
+    average_position_contracts_dict = (
+        calculate_position_series_given_variable_risk_for_dict(
+            capital=capital,
+            risk_target_tau=risk_target_tau,
+            idm=idm,
+            weights=instrument_weights,
+            std_dev_dict=std_dev_dict,
+            fx_series_dict=fx_series_dict,
+            multipliers=multipliers,
         )
-        for fast_span in fast_spans
-    ]
+    )
 
-    ### NOTE: This assumes we are equally weighted across spans
-    ### eg all forecast weights the same, equally weighted
-    all_forecasts_as_df = pd.concat(all_forecasts_as_list, axis=1)
-    average_forecast = all_forecasts_as_df.mean(axis=1)
+    ## We use three arbitrary slow spans here for both instruments
+    ## In reality we would need to check costs and turnover
+    fast_spans = [16, 32, 64]
+    position_contracts_dict = (
+        calculate_position_dict_with_multiple_trend_forecast_applied(
+            adjusted_prices_dict=adjusted_prices_dict,
+            average_position_contracts_dict=average_position_contracts_dict,
+            std_dev_dict=std_dev_dict,
+            fast_spans=fast_spans,
+        )
+    )
 
-    ## apply an FDM
-    rule_count = len(fast_spans)
-    FDM_DICT = {1: 1.0, 2: 1.03, 3: 1.08, 4: 1.13, 5: 1.19, 6: 1.26}
-    fdm = FDM_DICT[rule_count]
+    buffered_position_dict = apply_buffering_to_position_dict(
+        position_contracts_dict=position_contracts_dict,
+        average_position_contracts_dict=average_position_contracts_dict,
+    )
 
-    scaled_forecast = average_forecast * fdm
-    capped_forecast = scaled_forecast.clip(-20, 20)
+    perc_return_dict = calculate_perc_returns_for_dict_with_costs(
+        position_contracts_dict=buffered_position_dict,
+        fx_series=fx_series_dict,
+        multipliers=multipliers,
+        capital=capital,
+        adjusted_prices=adjusted_prices_dict,
+        cost_per_contract_dict=cost_per_contract_dict,
+        std_dev_dict=std_dev_dict,
+    )
 
-    return capped_forecast
+    return buffered_position_dict
+
+
+multipliers = dict(sp500=5, us10=1000)
+risk_target_tau = 0.2
