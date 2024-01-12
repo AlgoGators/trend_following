c4ab75319ddb795e2af13d5e1d22995b4cd75b67
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-01-11 20:02:00 -0500

    fin risk functions seperation

--- risk_functions.py
+++ risk_functions.py
@@ -127,3 +127,50 @@ def calculate_percentage_returns(
 def calculate_daily_returns(adjusted_price: pd.Series) -> pd.Series:
 
     return adjusted_price.diff()
+
+def calculate_position_series_given_variable_risk_for_dict(
+    capital: float,
+    risk_target_tau: float,
+    idm: float,
+    weights: dict,
+    fx_series_dict: dict,
+    multipliers: dict,
+    std_dev_dict: dict,
+) -> dict:
+
+    position_series_dict = dict(
+        [
+            (
+                instrument_code,
+                calculate_position_series_given_variable_risk(
+                    capital=capital * idm * weights[instrument_code],
+                    risk_target_tau=risk_target_tau,
+                    multiplier=multipliers[instrument_code],
+                    fx=fx_series_dict[instrument_code],
+                    instrument_risk=std_dev_dict[instrument_code],
+                ),
+            )
+            for instrument_code in std_dev_dict.keys()
+        ]
+    )
+
+    return position_series_dict
+
+def calculate_position_series_given_variable_risk(
+    capital: float,
+    risk_target_tau: float,
+    fx: pd.Series,
+    multiplier: float,
+    instrument_risk: standardDeviation,
+) -> pd.Series:
+
+    # N = (Capital × τ) ÷ (Multiplier × Price × FX × σ %)
+    ## resolves to N = (Capital × τ) ÷ (Multiplier × FX × daily stdev price terms × 16)
+    ## for simplicity we use the daily risk in price terms, even if we calculated annualised % returns
+    daily_risk_price_terms = instrument_risk.daily_risk_price_terms()
+
+    return (
+        capital
+        * risk_target_tau
+        / (multiplier * fx * daily_risk_price_terms * (BUSINESS_DAYS_IN_YEAR ** 0.5))
+    )
